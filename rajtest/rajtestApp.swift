59cadb42bc0662853a5f8d89cdf62686bcf64bac
Nikunj Thakur <nikunj.thakur@thescore.com>
2024-11-19 20:30:58 -0500

    Initial Commit

---
+++ rajtest/rajtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  rajtestApp.swift
+//  rajtest
+//
+//  Created by Nikunj Thakur on 2024-11-19.
+//
+
+import SwiftUI
+
+@main
+struct rajtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
